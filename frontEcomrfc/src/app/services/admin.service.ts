86876da1dfd7811b0c9037df9c0a6bf5210a6d8f
MedAzizOueslati <oueslati.mohamedaziz@esprit.tn>
2024-06-04 16:22:02 +0000

    sonar1

--- frontEcomrfc/src/app/services/admin.service.ts
+++ frontEcomrfc/src/app/services/admin.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { UserStorageService } from './storage/user-storage.service';
 
 const BASIC_URL = "http://localhost:8089/api/"
