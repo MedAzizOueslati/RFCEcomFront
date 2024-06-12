254c79ccf247f1c084bfea8fe007ff52474eab21
MedAzizOueslati <oueslati.mohamedaziz@esprit.tn>
2024-06-12 12:14:03 +0100

    kramcov3

--- frontEcomrfc/karma.conf.js
+++ frontEcomrfc/karma.conf.js
@@ -1,30 +1,38 @@
-module.exports = function(config) {
-    config.set({
-      // Other configuration settings
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        require('karma-jasmine-html-reporter'),
-        require('karma-coverage'),
-        require('@angular-devkit/build-angular/plugins/karma')
-      ],
-      coverageReporter: {
-        type: 'lcov',
-        dir: require('path').join(__dirname, './coverage'),
-        subdir: '.',
-        includeAllSources: true
-      },
-      reporters: ['progress', 'coverage', 'kjhtml'],
-      browsers: ['ChromeHeadless'],
-      customLaunchers: {
-        ChromeHeadless: {
-          base: 'ChromeHeadless',
-          flags: ['--no-sandbox', '--disable-gpu']
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        emitWarning: false,
+        global: {
+          statements: 0,
+          lines: 0,
+          branches: 0,
+          functions: 0
         }
-      },
-      singleRun: true,
-      restartOnFileChange: true
-    });
-  };
-  
+      }
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
